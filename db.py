470fad6906508120056cc4b3010b2221c4c1ec5f
Kairat <talantbekov123@gmail.com>
2017-12-06 13:42:27 +0600

    Add axilary functions to db module

--- db.py
+++ db.py
@@ -1,22 +1,22 @@
 import sqlite3
-
+from random import randint
 def create_tables():
     conn = sqlite3.connect('test.db')
     conn.execute('''CREATE TABLE IF NOT EXISTS users(
-                id INTEGER PRIMARY KEY,
-                chat_id INT,
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                chat_id INT UNIQUE,
                 verified BOOLEAN,
                 email VARCHAR(50)
             );''')
 
     conn.execute('''CREATE TABLE IF NOT EXISTS chats(
-                id INT PRIMARY KEY NOT NULL,
-                fist INT NOT NULL,
-                second INT NOT NULL
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                first INT,
+                second INT
             );''')
 
     conn.execute('''CREATE TABLE IF NOT EXISTS questions(
-                id INT PRIMARY KEY NOT NULL,
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 value INT NOT NULL
             );''')
 
@@ -41,15 +41,102 @@ def drop_tables():
 
 def find_user(chat_id):
     conn = sqlite3.connect('test.db')
-    print(chat_id)
     rows = conn.execute('SELECT * FROM users WHERE chat_id=?', (chat_id,))
-    rows = rows.fetchall()
+    row = rows.fetchone()
     conn.close()
-    return rows
-
-def create_user(chat_id,verified,email):
+    return row
+# if already exists returns false
+def create_user(chat_id, verified, email):
+    res = find_user(chat_id)
     conn = sqlite3.connect('test.db')
-    print(type(chat_id))
+
+    if(res == None):
+        conn.executemany("insert into users (chat_id, verified, email) values (?,?,?);", [(chat_id, verified, email,),])
+        conn.commit()
+        conn.close()
+        return True
+    else:
+        conn.close();
+        return False
+
+# update user, second and third values are change values
+def update_user(chat_id, verified, email):
+    res = find_user(chat_id)
+    conn = sqlite3.connect('test.db')
+    if(res != None):
+        conn.executemany("UPDATE users SET verified = ?, email = ? WHERE chat_id = ?", [(verified, email, chat_id,),])
+        conn.commit()
+        conn.close()
+        return True
+    else:
+        conn.close();
+        return False
     conn.executemany("insert into users (chat_id, verified, email) values (?,?,?);", [(chat_id, verified, email,),])
+    
+
+
+def create_chat(first, second):
+    conn = sqlite3.connect('test.db')
+    conn.executemany("insert into chats (first, second) values (?,?);", [(first, second,),])
     conn.commit()
+    conn.close()
+
+
+def show_users():
+    conn = sqlite3.connect('test.db')
+    rows = conn.execute('SELECT * FROM users')
+    rows = rows.fetchall()
+    print(rows)
+    conn.close()
+
+def show_chats():
+    conn = sqlite3.connect('test.db')
+    rows = conn.execute('SELECT * FROM chats')
+    rows = rows.fetchall()
+    print(rows)
+    conn.close()
+
+def is_verified(chat_id):
+    res = find_user(chat_id)
+    conn = sqlite3.connect('test.db')
+
+    if(res == None):
+        conn.close();
+        return False
+    else:
+        return bool(res[2])
+
+def remove_chat(first, second):
+    conn = sqlite3.connect('test.db')
+    conn.execute("delete from chats where first = (?);", (first,))
+    conn.execute("delete from chats where second = (?);", (second,))
+    conn.commit()
+    conn.close()
+
+def create_question(value):
+    conn = sqlite3.connect('test.db')
+    conn.execute("insert into questions (value) values (?);", (value,))
+    conn.commit()
+    conn.close()
+
+def show_questions():
+    conn = sqlite3.connect('test.db')
+    rows = conn.execute('SELECT * FROM questions')
+    rows = rows.fetchall()
+    print(rows)
+    conn.close()
+
+# returns 10 top questions
+def get_questions():
+    conn = sqlite3.connect('test.db')
+    rows = conn.execute("select count(*) from questions")
+    rows_num = rows.fetchone()[0]
+    res = []
+    while(len(res) != 3):
+        elem = randint(1, 6)
+        print(elem)
+        rows = conn.execute('SELECT * FROM questions WHERE id=?', (elem,))
+        row = rows.fetchone()
+        res.append(row)
+    print(res)
     conn.close()
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import telebot
 import db
-db.drop_tables();
+db.drop_tables()
 db.create_tables();
-db.create_user(123,False,"talantbekov_k@auca.kg")
-print("XX",db.find_user(123))
-'''
 token = "491935642:AAH8bboAvOR4VwInhs0QzCoUPH8hxC7FHSY"
 bot = telebot.TeleBot(token)
 
+
+db.create_question("some data");
+db.create_question("A");
+db.create_question("B");
+db.create_question("V");
+db.create_question("C");
+db.create_question("D");
+
+db.show_questions();
+db.get_questions()
+
+'''
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    print(message.chat.id)
-    db.create_tables()
-    temp = db.find_user(message.chat.id)
-    print(temp)
+    if(db.isVerified(message.chat.id))
+        db.create_user(message.chat.id, False, "None")
+        
+    db.update_user(message.chat.id, True, "Hello")
+    print("F2",db.isVerified(message.chat.id))
+
+    db.show_users()
     bot.send_message(message.chat.id, "AUCA response")
 
 bot.polling(none_stop=False, interval = 0)
